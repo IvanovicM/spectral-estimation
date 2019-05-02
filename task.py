1f6ea4afcb09a64a65cde84446c1419a988e93c8
IvanovicM <ivanovicmimi@gmail.com>
2019-05-02 17:57:28 +0200

    Experiments with different order in parametric covariance method

--- task.py
+++ task.py
@@ -136,17 +136,47 @@ def apply_and_plot_all(x):
     plt.ylabel('P')
     plt.show()
 
+def apply_and_plot_with_order(x, p):
+    cov = CovarianceMethod()
+    
+    for p_i in p:
+        _, axarr = plt.subplots(1, 2)
+        var = np.zeros(np.shape(x)[0])
+
+        # Plot estimations for current p_i
+        for i in range(np.shape(x)[0]):
+            print('Estimating:', i)
+            cov.estimate(x[i][:], p=p_i)
+
+            axarr[0].semilogy(cov['f'], cov['P'])
+            var[i] = cov['var_u']
+
+        # Label subplots
+        axarr[0].set_title('Covariance method for p = {}'.format(p_i))
+        axarr[0].set(xlabel='f [Hz]', ylabel='P [dB]')
+
+        axarr[1].stem(var)
+        axarr[1].set_title('Variance for p = {}'.format(p_i))
+        axarr[1].set(xlabel='realisation', ylabel='var')
+        
+        # Show current plot
+        plt.show()
+
 if __name__ == '__main__':
     # Read data
     x = read_data('data/data.csv', delimiter=',', file_size=[50, 256])
     #plot_realisations(x, num=2)
 
-    # Apply various method for spectral estimation
+    # 1-2. Apply various methods for spectral estimation
     #apply_classical_methods(x)
     #apply_parametric_methods(x)
 
-    # Apply window closing and show results
+    # 3. Apply window closing and show results
     #window_closing_on_blackman_tukey(x)
 
-    # Apply all realizations
-    apply_and_plot_all(x)
+    # 7. Apply a few methods on all realisations
+    #apply_and_plot_all(x)
+
+    # 9. Apply Covariance method with different orders.
+    N = 256
+    apply_and_plot_with_order(x, [N//2, N//4])
