3b0546f80841a25cfe22f3e32f587f3afb30e3a6
IvanovicM <ivanovicmimi@gmail.com>
2019-05-03 19:19:49 +0200

    Filtering input with A(z) from parametric methods

--- task.py
+++ task.py
@@ -3,10 +3,12 @@ import csv
 import seaborn as sns
 import matplotlib.pyplot as plt
 from random import randint
+from scipy import signal
 
 from classical.Periodogram import Periodogram
 from classical.AveragedPeriodogram import AveragedPeriodogram
 from classical.BlackmanTukey import BlackmanTukey
+from classical.Autocorrelation import Autocorrelation
 from parametric.AutocorrelationMethod import AutocorrelationMethod
 from parametric.CovarianceMethod import CovarianceMethod
 from parametric.ModifiedCovarianceMethod import ModifiedCovarianceMethod
@@ -42,49 +44,49 @@ def plot_realisations(x, num=1):
 
 def apply_classical_methods(x):
     # Periodogram
-    rand_index = randint(0, np.shape(x)[0])
+    rand_index = randint(0, np.shape(x)[0] - 1)
     per = Periodogram()
     per.estimate(x[rand_index][:])
     per.plot()
     per.compare(x[rand_index][:])
 
     # Averaged periodogram
-    rand_index = randint(0, np.shape(x)[0])
+    rand_index = randint(0, np.shape(x)[0] - 1)
     avg_per = AveragedPeriodogram()
     avg_per.estimate(x[rand_index][:], L=50)
     avg_per.plot()
     avg_per.compare(x[rand_index][:], L=50)
 
     # Blackman-Tukey
-    rand_index = randint(0, np.shape(x)[0])
+    rand_index = randint(0, np.shape(x)[0] - 1)
     bm = BlackmanTukey()
     bm.estimate(x[rand_index][:], M=50)
     bm.plot()
 
 def apply_parametric_methods(x):
     # Autocorrelaton method
-    rand_index = randint(0, np.shape(x)[0])
+    rand_index = randint(0, np.shape(x)[0] - 1)
     autocorr = AutocorrelationMethod()
-    autocorr.estimate(x[rand_index][:], p=5)
+    autocorr.estimate(x[rand_index][:], p=15)
     autocorr.plot()
 
     # Covariance method
-    rand_index = randint(0, np.shape(x)[0])
+    rand_index = randint(0, np.shape(x)[0] - 1)
     cov = CovarianceMethod()
-    cov.estimate(x[rand_index][:], p=5)
+    cov.estimate(x[rand_index][:], p=15)
     cov.plot()
     
     # Modified covariance method
-    rand_index = randint(0, np.shape(x)[0])
+    rand_index = randint(0, np.shape(x)[0] - 1)
     mod_cov = ModifiedCovarianceMethod()
-    mod_cov.estimate(x[rand_index][:], p=5)
+    mod_cov.estimate(x[rand_index][:], p=15)
     mod_cov.plot()
 
     # Burg method
     # TODO
 
 def window_closing_on_blackman_tukey(x):
-    rand_index = randint(0, np.shape(x)[0])
+    rand_index = randint(0, np.shape(x)[0] - 1)
     xr = x[rand_index][:]
 
     # Window closing and ploting
@@ -162,21 +164,59 @@ def apply_and_plot_with_order(x, p):
         # Show current plot
         plt.show()
 
+def filter_and_autocorr(x):
+    rand_index = randint(0, np.shape(x)[0] - 1)
+    cov = CovarianceMethod()
+    cov.estimate(x[rand_index][:], p=15)
+    
+    # Apply filter
+    b = np.ndarray.flatten(cov['a'])
+    a = [1]
+    y = signal.filtfilt(b, a, x[rand_index][:])
+
+    # Plot result
+    plt.figure()
+    plt.plot(y)
+    plt.title('Fitered signal')
+    plt.xlabel('t [s]')
+    plt.show()
+
+    # Plot autocorrelation of the result
+    autocorr = Autocorrelation()
+    autocorr.estimate(y)
+    autocorr.plot()
+
+    # Plot autocorrelation on some segment
+    r_yy = np.zeros(61)
+    k = 0
+    for i in np.arange(-30, 31):
+        r_yy[k] = autocorr[i]
+        k += 1
+    plt.figure()
+    plt.plot(np.arange(-30, 31), r_yy)
+    plt.title('Autocorrelation of filtered signal')
+    plt.xlabel('n')
+    plt.show()
+
 if __name__ == '__main__':
     # Read data
     x = read_data('data/data.csv', delimiter=',', file_size=[50, 256])
     #plot_realisations(x, num=2)
 
-    # 1-2. Apply various methods for spectral estimation
+    # 1. 2. 4. Apply various methods for spectral estimation
     #apply_classical_methods(x)
     #apply_parametric_methods(x)
 
     # 3. Apply window closing and show results
     #window_closing_on_blackman_tukey(x)
 
+    # 6. Filter sequence and show autocorrelation onf the result.
+    filter_and_autocorr(x)
+
     # 7. Apply a few methods on all realisations
     #apply_and_plot_all(x)
 
     # 9. Apply Covariance method with different orders.
-    N = 256
-    apply_and_plot_with_order(x, [N//2, N//4])
+    #N = 256
+    #apply_and_plot_with_order(x, [N//2, N//4])
+    
