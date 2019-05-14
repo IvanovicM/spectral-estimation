3a7b523e05adac317d15bf0e8272e2da420fd749
IvanovicM <ivanovicmimi@gmail.com>
2019-05-14 22:37:41 +0200

    Ploting with variance reorg in one function

--- task.py
+++ task.py
@@ -87,6 +87,40 @@ def apply_parametric_methods(x):
     # Burg method
     # TODO
 
+def plot_all_with_variance(estimator, x, title_0, title_1, p=None, M=None):
+    Nr = np.shape(x)[0]
+    N = np.shape(x)[1]
+    mv = MeanAndVar()
+    f_len = 100
+    f = np.linspace(0, 0.5, f_len)
+    estimated_P = np.zeros(shape=[Nr, f_len])
+
+    _, axarr = plt.subplots(1, 2)
+    for nr in range(Nr):
+        # Plot estimated P.
+        print('Estimating:', nr)
+        if p is None:
+            if M is None:
+                estimator.estimate(x[nr][:], f=f)
+            else:
+                estimator.estimate(x[nr][:], f=f, M=M)
+        else:
+            estimator.estimate(x[nr][:], f=f, p=p)
+        
+        axarr[0].semilogy(f, estimator['P'])
+        estimated_P[nr][:] = estimator['P']
+
+    # Plot variance.
+    mv.estimate(estimated_P)
+    axarr[1].plot(f, mv['var'])
+
+    # Label subplots & show
+    axarr[0].set_title(title_0)
+    axarr[0].set(xlabel='f [Hz]', ylabel='P [dB]')
+    axarr[1].set_title(title_1)
+    axarr[1].set(xlabel='f [Hz]', ylabel='var')
+    plt.show()
+
 def window_closing_on_blackman_tukey(x):
     rand_index = randint(0, np.shape(x)[0] - 1)
     xr = x[rand_index][:]
@@ -105,76 +139,29 @@ def window_closing_on_blackman_tukey(x):
 
 def apply_and_plot_all(x):
     # Periodogram
-    per = Periodogram()
-    plt.figure()
-    for i in range(np.shape(x)[0]):
-        print('Estimating:', i)
-        per.estimate(x[i][:])
-        plt.semilogy(per['f'], per['P'])
-    plt.title('Periodogram on all realisations')
-    plt.xlabel('f [Hz]')
-    plt.ylabel('P')
-    plt.show()
+    plot_all_with_variance(Periodogram(), x,
+                           'Periodogram on all realisations',
+                           'Variance for periodogram')
 
     # CovarianceMethod
-    cov = CovarianceMethod()
-    plt.figure()
-    for i in range(np.shape(x)[0]):
-        print('Estimating:', i)
-        cov.estimate(x[i][:], p=5)
-        plt.semilogy(cov['f'], cov['P'])
-    plt.title('Covariance method on all realisations')
-    plt.xlabel('f [Hz]')
-    plt.ylabel('P')
-    plt.show()
+    plot_all_with_variance(CovarianceMethod(), x,
+                           'Covariance method on all realisations',
+                           'Variance for covariance method',
+                           p=5)
 
     # Blackman-Tukey
-    bm = BlackmanTukey()
-    plt.figure()
-    for i in range(np.shape(x)[0]):
-        print('Estimating:', i)
-        bm.estimate(x[i][:], M=10)
-        plt.semilogy(bm['f'], bm['P'])
-    plt.title('Blackman-Tukey on all realisations')
-    plt.xlabel('f [Hz]')
-    plt.ylabel('P')
-    plt.show()
+    plot_all_with_variance(BlackmanTukey(), x,
+                           'BlackMan-Tukey method on all realisations',
+                           'Variance for BlackMan-Tukey method',
+                           M=10)
 
-def apply_and_plot_with_order(x, p):
-    Nr = np.shape(x)[0]
-    N = np.shape(x)[1]
-    cov = CovarianceMethod()
-    mv = MeanAndVar()
-
-    f_len = 100
-    f = np.linspace(0, 0.5, f_len)
-    
+def apply_and_plot_with_order(x, p):  
+    cov = CovarianceMethod()  
     for p_i in p:
-        _, axarr = plt.subplots(1, 2)
-        var = np.zeros(np.shape(x)[0])
-
-        # Estimate and plot P for current p_i
-        estimated_P = np.zeros(shape=[Nr, f_len])
-        for nr in range(np.shape(x)[0]):
-            print('Estimating:', nr)
-            cov.estimate(x[nr][:], f=f, p=p_i)
-
-            axarr[0].semilogy(f, cov['P'])
-            estimated_P[nr][:] = cov['P']
-
-        # Plot variance.
-        mv.estimate(estimated_P)
-        axarr[1].plot(f, mv['var'])
-
-        # Label subplots
-        axarr[0].set_title('Covariance method for p = {}'.format(p_i))
-        axarr[0].set(xlabel='f [Hz]', ylabel='P [dB]')
-
-        axarr[1].set_title('Variance for p = {}'.format(p_i))
-        axarr[1].set(xlabel='f [Hz]', ylabel='var')
-        
-        # Show current plot
-        plt.show()
+        plot_all_with_variance(cov, x,
+                               'Covariance method for p = {}'.format(p_i),
+                               'Variance for p = {}'.format(p_i),
+                               p=p_i)
 
 def show_variance_for_covariance_method(x, p):
     Nr = np.shape(x)[0]
@@ -258,12 +245,12 @@ if __name__ == '__main__':
     #filter_and_autocorr(x)
 
     # 7. Apply a few methods on all realisations
-    #apply_and_plot_all(x)
+    apply_and_plot_all(x)
 
     # 8. Show estimated variance for Covariance method.
     #show_variance_for_covariance_method(x, 10)
 
     # 9. Apply Covariance method with different orders.
-    N = 256
-    apply_and_plot_with_order(x, [N // 2, N // 4])
+    #N = 256
+    #apply_and_plot_with_order(x, [N // 2, N // 4])
     
