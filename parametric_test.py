bb0db7e9500bd9eaa4fe8ea5dde434cddf270d6c
IvanovicM <ivanovicmimi@gmail.com>
2019-06-12 21:05:05 +0200

    Test for utils moved to other file

--- parametric_test.py
+++ parametric_test.py
@@ -5,8 +5,6 @@ from parametric.AutocorrelationMethod import AutocorrelationMethod
 from parametric.CovarianceMethod import CovarianceMethod
 from parametric.ModifiedCovarianceMethod import ModifiedCovarianceMethod
 from parametric.Burg import Burg
-from utils.MeanAndVar import MeanAndVar
-from utils.ModelOrderSelector import ModelOrderSelector
 
 def autocorrelation_method_test():
     t = np.arange(1000)
@@ -64,39 +62,9 @@ def burg_method_test():
     burg.estimate(np.add(x, u), p=10)
     burg.plot()
 
-def model_order_selector_test():
-    N = 1000
-    t = np.arange(N)
-    x = np.sin(1*t + 2.8) + np.sin(2*t + 3.4)
-    u = np.random.normal(size=N)
-
-    mv = MeanAndVar()
-    selector = ModelOrderSelector()
-    autocorr = ModifiedCovarianceMethod()
-
-    # Estimation + variance for various p
-    max_p = 2
-    rho = np.zeros(max_p + 1)
-    for p in np.arange(1, max_p + 1):
-        autocorr.estimate(np.add(x, u), p=p)
-        mv.estimate(np.transpose(autocorr['P']))
-        rho[p] = mv['var']
-
-    # Apply order selection
-    selector.apply('FPE', N, max_p, rho)
-    selector.plot()
-
-def mean_var_test():
-    x = np.random.normal(size=[100, 150])
-    mv = MeanAndVar()
-    mv.estimate(x)
-    mv.plot(np.arange(-200, -50))
-
 if __name__ == "__main__":
     #autocorrelation_test()
     #autocorrelation_method_test()
     #covariance_method_test()
     #modified_covariance_method_test()
     burg_method_test()
-    #mean_var_test()
-    #model_order_selector_test()
---
+++ utils_test.py
@@ -0,0 +1,38 @@
+import numpy as np
+from scipy import signal
+from parametric.CovarianceMethod import CovarianceMethod 
+from utils.MeanAndVar import MeanAndVar
+from utils.ModelOrderSelector import ModelOrderSelector
+
+def model_order_selector_test():
+    N = 256
+    t = np.arange(N)
+    u = np.random.normal(size=N)
+
+    b = [2,3]
+    a = [1,0.2]
+    x = signal.lfilter(b, a, u)
+
+    selector = ModelOrderSelector()
+    cov = CovarianceMethod()
+
+    # Estimation + variance for various p
+    max_p = 20
+    rho = np.zeros(max_p + 1)
+    for p in np.arange(1, max_p + 1):
+        cov.estimate(x, p=p)
+        rho[p] = cov['var_u']
+
+    # Apply order selection
+    selector.apply('CAT', N, max_p, rho)
+    selector.plot()
+
+def mean_var_test():
+    x = np.random.normal(size=[100, 150])
+    mv = MeanAndVar()
+    mv.estimate(x)
+    mv.plot(np.arange(-200, -50))
+
+if __name__ == "__main__":
+    #mean_var_test()
+    model_order_selector_test()
